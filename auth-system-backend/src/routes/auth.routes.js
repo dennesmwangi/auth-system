d3f682246d30dfe318b851c631edf6da8a9f4113
Dennes Njoroge <dennesmwangi55@gmail.com>
2026-04-27 09:31:34 +0300

    add auth to change password route

--- auth-system-backend/src/routes/auth.routes.js
+++ auth-system-backend/src/routes/auth.routes.js
@@ -20,6 +20,6 @@ router.post("/logout", logoutUser);
 router.post("/forgot", forgotPassword);
 router.post("/verify-reset-code", verifyResetCode);
 router.post("/reset-password", resetPassword);
-router.post("/change-password", changePassword);
+router.post("/change-password", auth, changePassword);
 
 export default router;
