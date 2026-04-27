ff71ba4dfe641ca5202d0d402cbb403125535f39
Dennes Njoroge <dennesmwangi55@gmail.com>
2026-04-27 12:40:18 +0300

    feature: update profile

--- auth-system-backend/src/routes/user.routes.js
+++ auth-system-backend/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { auth } from "../auth/auth.js";
 import db from "../config/db.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -25,6 +26,42 @@ router.get("/dashboard", auth, async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
+});
+
+router.post("/update", auth, async (req, res) => {
+  const { firstName, lastName, phoneNumber } = req.body;
+  const token = req.cookies.loginToken;
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  //const userId = req.id;
+  const userId = decoded.id;
+  //console.log(userId);
+
+  /* return res.status(200).json({
+    firstName: firstName,
+    lastName: lastName,
+    phoneNumber: phoneNumber,
+    user_id: userId,
+  });*/
+  try {
+    const [rows] = await db.execute(`SELECT id FROM users WHERE id = ?`, [
+      userId,
+    ]);
+
+    if (rows.length === 0) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    const user = rows[0];
+
+    await db.execute(
+      `UPDATE users SET first_name = ?, last_name = ?, phone_number = ? WHERE id = ?`,
+      [firstName, lastName, phoneNumber, userId],
+    );
+
+    return res.status(200).json({ message: "Profile updated successfully" });
+  } catch (error) {}
 });
 
 export default router;
