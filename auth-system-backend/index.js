d8c99b9f1057c9d9d7bbfd16c54271020466d2fa
Dennes Njoroge <dennesmwangi55@gmail.com>
2026-04-27 08:40:16 +0300

    Debug

--- auth-system-backend/index.js
+++ auth-system-backend/index.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 import db from "./src/config/db.js";
 import authRoutes from "./src/routes/auth.routes.js";
 import userRoutes from "./src/routes/user.routes.js";
+import { json } from "stream/consumers";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 5000;
@@ -29,6 +30,12 @@ app.use(
 app.use(express.json({ limit: "1mb" }));
 
 app.get("/", (req, res) => {
+  console.log({
+    ip: req.socket.remoteAddress,
+    port: req.socket.remotePort,
+    browser: req.headers["user-agent"],
+    language: req.headers["accept-language"],
+  });
   res.status(200).json({
     name: "auth-system-backend",
     version: "1.0.0",
